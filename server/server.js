80f945b81df3497eddd41897954d71e91cb4758a
pragyesh007 <koolthebossch@gmail.com>
2019-02-09 23:17:36 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -56,10 +56,31 @@ app.get('/todos' , (req,res) =>
   }).catch((e) => res.status(400).send());
   });
 
+
+  app.delete('/todos/:id' , (req,res) =>
+   {
+     var id = req.params.id;
+     if(!ObjectID.isValid(id))
+     {
+       return res.status(404).send();
+     }
+
+     todo.findByIdAndRemove(id).then( (todo) =>
+     {
+       if(!todo)
+       {
+           return res.status(404).send();
+       }
+       res.send({todo});
+     }).catch((e) => res.status(400).send());
+
+     });
+
+
 app.listen(port, () =>
 {
   console.log(`started at port ${port}`);
-})
+});
 
 module.exports =
  {
